d5a3aef2531f032f855a004f99ebd88a1bbb6b55
li_j604 <775837740@qq.com>
2021-12-02 15:40:28 +0800

    更新book/detail Mock路由

---
+++ src/mock/bookDetails.js
@@ -0,0 +1,24 @@
+module.exports = {
+  error_code: 0,
+  msg: "获取成功",
+  data: {
+    id: 3,
+    fileName: "2018_Book_PeerReviewPeerEducationAndMode",
+    cover:
+      "https://47.99.166.157/book/res/img//Philosophy/978-3-319-90955-4_CoverFigure.jpg",
+    title:
+      "Peer Review, Peer Education, and Modeling in the Practice of Clinical Ethics Consultation: The Zadeh Project",
+    author: "Stuart G. Finder",
+    publisher: "Springer International Publishing",
+    bookId: "2018_Book_PeerReviewPeerEducationAndMode",
+    category: 18,
+    categoryText: "Philosophy",
+    language: "en",
+    rootFile: "OEBPS/package.opf",
+    selected: false,
+    private: false,
+    cache: false,
+    haveRead: 0,
+    type: 1
+  }
+};
